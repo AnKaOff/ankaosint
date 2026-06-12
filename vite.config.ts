0afce4060238cd3dc87e9fded557f676f313141c
ankaosint <ankagfx@gmail.com>
2026-06-12 21:33:50 +0200

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,27 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { createServer } from "./server";
 
-export default {
-  base: "/ankaosint/",
-}
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    fs: {
+      allow: ["./client", "./shared", "index.html"],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+    },
+  },
+  build: {
+    outDir: "dist/spa",
+  },
+  plugins: [react(), expressPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
+}));
 
 
 function expressPlugin(): Plugin {
